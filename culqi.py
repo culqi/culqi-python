3c9e0c9fb6d44b533e17c087712b5422ce1162d1
Willy Aguirre <marti1125@gmail.com>
2017-01-03 18:47:37 -0500

    add charges and plans

--- culqi.py
+++ culqi.py
@@ -9,6 +9,11 @@ class Culqi:
         self.COD_COMMERCE = COD_COMMERCE
         self.API_KEY = API_KEY
 
+    def jsonResult(self, url, data):
+        headers = {"Authorization": "Bearer "+self.API_KEY, "content-type": "application/json"}
+        r = requests.post(self.API_URL+url, headers=headers, data=data)
+        return r.content
+
     def createToken(self, card_number, currency_code, cvv, exp_month, exp_year, fingerprint, last_name, email, first_name):
         token = Object()
         token.card_number = card_number
@@ -23,3 +28,33 @@ class Culqi:
         headers = {"Authorization": "Code "+self.COD_COMMERCE, "content-type": "application/json"}
         r = requests.post(self.API_URL+"/tokens/", headers=headers, data=token.toJSON())
         return r.content
+
+    def createCharge(self, address, address_city, amount, country_code, currency_code, email, first_name, installments, last_name,
+                     order_id, phone_number, product_description, token_id):
+        charge = Object()
+        charge.address = address
+        charge.address_city = address_city
+        charge.amount = amount
+        charge.country_code = country_code
+        charge.currency_code = currency_code
+        charge.email = email
+        charge.first_name = first_name
+        charge.installments = installments
+        charge.last_name = last_name
+        charge.order_id = order_id
+        charge.phone_number = phone_number
+        charge.product_description = product_description
+        charge.token_id = token_id
+        return self.jsonResult("/charges/", charge.toJSON())
+
+    def createPlan(self, alias, amount, currency_code, interval, interval_count, limit, name, trial_days):
+        plan = Object()
+        plan.alias = alias
+        plan.amount = amount
+        plan.currency_code = currency_code
+        plan.interval = interval
+        plan.interval_count = interval_count
+        plan.limit = limit
+        plan.name = name
+        plan.trial_days = trial_days
+        return self.jsonResult("/plans/", plan.toJSON())
--- example.py
+++ example.py
@@ -3,10 +3,13 @@ from culqi import Culqi
 
 def main():
     culqi = Culqi("pk_test_vzMuTHoueOMlgUPj","sk_test_UTCQSGcXW8bCyU59")
-    data = json.loads(culqi.createToken(
+    token = json.loads(culqi.createToken(
         "4111111111111111","PEN","123",9,2020,"q352454534","Muro","wmuro@me.com","William"
     ))
-    print data["id"]
+    print token["id"]
+
+    plan = json.loads(culqi.createPlan("plan-test-CULQI107",1000,"PEN","day",2,10,"Plan de Prueba CULQI107",50))
+    print plan["alias"]
 
 if __name__ == "__main__":
     main()
