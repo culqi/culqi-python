9569f8ea2f415712b4e72424a3bf6d4b276f296d
Willy Aguirre <marti1125@gmail.com>
2017-01-05 18:10:19 -0500

    add tests

---
+++ culqi_py/test.py
@@ -0,0 +1,50 @@
+import unittest ,json, uuid
+from culqi import Culqi
+
+culqi  = Culqi("pk_test_vzMuTHoueOMlgUPj","sk_test_UTCQSGcXW8bCyU59")
+
+class TestStringMethods(unittest.TestCase):
+
+    culqi = Culqi("pk_test_vzMuTHoueOMlgUPj","sk_test_UTCQSGcXW8bCyU59")
+
+    def token(self):
+        token = json.loads(culqi.createToken(
+                "4111111111111111","PEN","123",9,2020,"q352454534","Muro","wmuro@me.com","William"
+                ))
+        return token
+
+    def charge(self):
+        charge = json.loads(culqi.createCharge("Avenida Lima 1232","LIMA",1000,"PE","PEN","wmuro@me.com","William",0,"Muro","",
+                 9899,3333339,"Venta de prueba",self.token()["id"]))
+        return charge
+
+    def plan(self):
+        plan = json.loads(culqi.createPlan("plan-test-"+str(uuid.uuid1()),1000,"PEN","day",2,10,"Plan de Prueba "+str(uuid.uuid1()),50))
+        return plan
+
+    def subscription(self):
+        subscription = json.loads(culqi.createSubscription("Avenida Lima 123213","LIMA","PE","wmuro@me.com","Muro","William",
+                       1234567789,self.plan()["alias"],self.token()["id"]))
+        return subscription
+
+    def refund(self):
+        refund = json.loads(culqi.createRefund(500,self.charge()["id"],"give me money back"))
+        return refund
+
+    def test_token(self):
+        self.assertEqual("token", self.token()["object"])
+
+    def test_charge(self):
+        self.assertEqual("charge", self.charge()["object"])
+
+    def test_plan(self):
+        self.assertEqual("plan", self.plan()["object"])
+
+    def test_subscription(self):
+        self.assertEqual("subscription", self.subscription()["object"])
+
+    def test_refund(self):
+        self.assertEqual("refund", self.refund()["object"])
+
+if __name__ == '__main__':
+    unittest.main()
