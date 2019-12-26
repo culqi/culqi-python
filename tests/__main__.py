98c3b34e04a47051454d268620069a248b7b55e6
zodiacfireworks <martin.vuelta@gmail.com>
2019-12-25 20:51:53 -0500

    Added __main__ to tests

---
+++ tests/__main__.py
@@ -0,0 +1,20 @@
+import unittest
+
+from .test_version import VersionTest
+from .test_client import ClientTest
+
+from .test_token import TokenTest
+from .test_card import CardTest
+from .test_charge import ChargeTest
+from .test_customer import CustomerTest
+from .test_event import EventTest
+from .test_iin import IinTest
+from .test_order import OrderTest
+from .test_plan import PlanTest
+from .test_refund import RefundTest
+from .test_suscription import SuscriptionTest
+from .test_transfer import TransferTest
+
+
+if __name__ == '__main__':
+    unittest.main()
