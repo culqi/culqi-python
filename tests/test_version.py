f77bdee83d6f6a688d92a5003d4107c072941291
Joel Ibaceta <mail@joelibaceta.com>
2020-01-03 16:17:27 -0500

    update version

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,7 +6,7 @@ from culqi import __version__
 class VersionTest(unittest.TestCase):
     @staticmethod
     def test_version():
-        assert __version__ == "1.0.2"
+        assert __version__ == "1.0.3"
 
 
 if __name__ == "__main__":
