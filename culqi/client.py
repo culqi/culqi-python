f4f669b50fb4d0d5f34bcae36814561bfc908f1d
Tekton <lorenaortizdezevallos@MacBook-Pro-de-Lorena.local>
2023-04-24 11:26:07 -0500

    fix: use encryption as util

--- culqi/client.py
+++ culqi/client.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from copy import deepcopy
 from types import ModuleType
 
@@ -7,12 +8,7 @@ from requests import session
 from . import resources
 from culqi.utils import capitalize_camel_case
 from culqi.version import VERSION
-
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-import base64
+import culqi.utils.encryption.rsa_aes.encoder as encoder
 
 RESOURCE_CLASSES = {}
 SCHEMAS = {}
@@ -23,6 +19,14 @@ for name, module in resources.__dict__.items():
     is_in_module = capitalized_name in getattr(module, "__dict__", {})
     if is_module and is_in_module:
         RESOURCE_CLASSES[name] = module.__dict__[capitalized_name]
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    rsa_aes_encoder = encoder.RsaAesEncoder()
+except Exception as e:
+    logger.error('Unable to create an instance of Base64Decoder class.', exc_info=True)
 
 class Culqi:
     def __init__(self, public_key, private_key):
@@ -74,73 +78,17 @@ class Culqi:
             del data["data"]
 
         return {"status": response.status_code, "data": data}
-    
-    def _encrypt(self, data, public_key):
-        # Generate a random encryption key
-        key = get_random_bytes(32)
-        iv = get_random_bytes(16)
-        
-        # Message to be encrypted
-        message = json.dumps(data).encode('utf-8')
-
-        # Pad the message to a multiple of 16 bytes using PKCS#7 padding
-        block_size = 16
-        padding_length = block_size - len(message) % block_size
-        padding = bytes([padding_length] * padding_length)
-        padded_message = message + padding
-
-
-        # Initialize the cipher with the key and IV
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-
-        # Encrypt the message
-        # Note that the message length must be a multiple of 16 bytes
-        ciphertext = cipher.encrypt(padded_message)
-        
-        encrypted_message = base64.b64encode(ciphertext).decode('utf-8')
-
-        # Encrypt the key with the public key
-        cipher = PKCS1_OAEP.new(RSA.import_key(public_key))
-        ciphertext_key = cipher.encrypt(key)
-
-        # Encrypt the iv with the public key
-        cipher = PKCS1_OAEP.new(RSA.import_key(public_key))
-        ciphertext_iv = cipher.encrypt(iv)
-
-        # Convert the encrypted message to a string
-        encrypted_aes_key = base64.b64encode(ciphertext_key).decode()
-        encrypted_aes_iv = base64.b64encode(ciphertext_iv).decode()
-
-        return {
-            "encrypted_data": encrypted_message, 
-            "encrypted_key": encrypted_aes_key, 
-            "encrypted_iv": encrypted_aes_iv
-        }
-    
-    def _encrypt_validation(self, data, options):
-        headers = {}
-        if("rsa_public_key" in options and "rsa_id" in options):
-            data = self._encrypt(data, options["rsa_public_key"])
-            headers["x-culqi-rsa-id"] = options["rsa_id"]
-            if "headers" in options:
-                options["headers"].update(headers)
-            else:
-                options["headers"] = headers
-            del options["rsa_public_key"]
-            del options["rsa_id"]
-        
-        return data, options
 
     def get(self, url, params, **options):
         return self.request("get", url, params=params, **options)
 
     def post(self, url, data, **options):
-        data, options = self._encrypt_validation(data, options)
+        data, options = rsa_aes_encoder.encrypt_validation(data, options)
         data, options = self._update_request(data, options) 
         return self.request("post", url, data, **options)
 
     def patch(self, url, data, **options):
-        data, options = self._encrypt_validation(data, options)
+        data, options = rsa_aes_encoder.encrypt_validation(data, options)
         data, options = self._update_request(data, options)
         return self.request("patch", url, data=data, **options)
 
---
+++ culqi/utils/encryption/rsa_aes/encoder.py
@@ -0,0 +1,64 @@
+import json
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import base64
+
+class RsaAesEncoder:
+
+    def _encrypt(self, data, public_key):
+        # Generate a random encryption key
+        key = get_random_bytes(32)
+        iv = get_random_bytes(16)
+        
+        # Message to be encrypted
+        message = json.dumps(data).encode('utf-8')
+
+        # Pad the message to a multiple of 16 bytes using PKCS#7 padding
+        block_size = 16
+        padding_length = block_size - len(message) % block_size
+        padding = bytes([padding_length] * padding_length)
+        padded_message = message + padding
+
+
+        # Initialize the cipher with the key and IV
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+
+        # Encrypt the message
+        # Note that the message length must be a multiple of 16 bytes
+        ciphertext = cipher.encrypt(padded_message)
+        
+        encrypted_message = base64.b64encode(ciphertext).decode('utf-8')
+
+        # Encrypt the key with the public key
+        cipher = PKCS1_OAEP.new(RSA.import_key(public_key))
+        ciphertext_key = cipher.encrypt(key)
+
+        # Encrypt the iv with the public key
+        cipher = PKCS1_OAEP.new(RSA.import_key(public_key))
+        ciphertext_iv = cipher.encrypt(iv)
+
+        # Convert the encrypted message to a string
+        encrypted_aes_key = base64.b64encode(ciphertext_key).decode()
+        encrypted_aes_iv = base64.b64encode(ciphertext_iv).decode()
+
+        return {
+            "encrypted_data": encrypted_message, 
+            "encrypted_key": encrypted_aes_key, 
+            "encrypted_iv": encrypted_aes_iv
+        }
+
+    def encrypt_validation(self, data, options):
+        headers = {}
+        if("rsa_public_key" in options and "rsa_id" in options):
+            data = self._encrypt(data, options["rsa_public_key"])
+            headers["x-culqi-rsa-id"] = options["rsa_id"]
+            if "headers" in options:
+                options["headers"].update(headers)
+            else:
+                options["headers"] = headers
+            del options["rsa_public_key"]
+            del options["rsa_id"]
+        
+        return data, options
